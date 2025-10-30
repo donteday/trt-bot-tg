02d11c5aa94f6a7dff3cab8210c57bf8f15539fb
Krukov Anton <meowskyj@yandex.ru>
2025-10-30 13:52:15 +0300

    fix prompt

--- handlers/textHandler.js
+++ handlers/textHandler.js
@@ -118,7 +118,6 @@ module.exports = function registerTextHandler(bot) {
 
       const userStyle = await db.getUserResponseStyle(userId);
       const prompt = buildPromptTarot(question, cards, userStyle);
-      console.log(prompt);
       
       let currentText = "🔮\n\n";
       let lastUpdate = Date.now();
